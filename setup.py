14f8eaae4e67cfe15cbde12135d44d6559b7de75
yuezht <751349974@qq.com>
2018-02-25 11:42:00 +0800

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='data-analysis',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='751349974@qq.com',
+    author_email='751349974@qq.com',
+    description='python:data analysis'
+)
