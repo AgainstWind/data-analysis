8081ce0008a98ed312db1033ac368f90c45259b7
yuezht <751349974@qq.com>
2018-03-12 16:16:41 +0800

    add test code

--- src/data/math_test.py
+++ src/data/math_test.py
@@ -27,3 +27,21 @@ a_eq = np.array([[3,-2,-3]])
 b_eq = np.array([-6])
 r = linprog(c,a_ub,b_ub,a_eq,b_eq,bounds=((None,0),(0,None),(None,None)))
 print(r)
+print("------------------------end---------------------------")
+
+"""
+线性规划demo2
+min: z = -4x1-x2
+s.t.:
+    -x1 + 2x2 <= 4
+    2x1 + 3x2 <= 12
+    x1 - x2 <= 3
+    x1>=0
+    x2>=0
+"""
+c2 = np.array([-4,-1])
+a_up2 = np.array([[-1,2],[2,3],[1,-1]])
+b_ub2 = np.array([4,12,3])
+r2 = linprog(c2,a_up2,b_ub2,bounds=((0,None),(0,None)))
+print(r2)
+print("------------------------end---------------------------")
