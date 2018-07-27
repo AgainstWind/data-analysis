9096a8690ff21ae66b0fea037cce15998b0b238f
yuezht <751349974@qq.com>
2018-07-27 23:47:30 +0800

    update

--- src/data/get_data_from_sina.py
+++ src/data/get_data_from_sina.py
@@ -1,9 +1,10 @@
 import urllib3
-import pandas
 
 '''
 create_table_stock_sina.sql
 '''
+data_header = ['symbol','code','name','trade','pricechange','changepercent','buy','sell','settlement','open','high',
+               'low','volume','amount','ticktime','per','pb','mktcap','nmc','turnoverratio' ]
 
 def sinaStockUrl(pageNum):
     print('pageNum : ' + str(pageNum))
@@ -15,20 +16,36 @@ def sinaStockUrl(pageNum):
     print(url)
     return url
 
-data_header = ['symbol','code','name','trade','pricechange','changepercent','buy','sell','settlement','open','high',
-               'low','volume','amount','ticktime','per','pb','mktcap','nmc','turnoverratio' ]
-
-
 def sinaStockData(url):
     http = urllib3.PoolManager()
     stockDataResponse = http.request('GET',url)
-    return stockDataResponse.data
+    return str(stockDataResponse.data,encoding='gbk')
 
+def parse_js(expr):
+    """
+    解析非标准JSON的Javascript字符串，等同于json.loads(JSON str)
+    :param expr:非标准JSON的Javascript字符串
+    :return:Python字典
+    """
+    obj = eval(expr, type('Dummy', (dict,), dict(__getitem__=lambda s, n: n))())
+    return obj
 
+'''
+b'[{symbol:"sh600000",code:"600000",name:"\xc6\xd6\xb7\xa2\xd2\xf8\xd0\xd0",trade:"9.990",pricechange:"0.030",
+changepercent:"0.301",buy:"9.990",sell:"10.000",settlement:"9.960",open:"9.960",high:"10.040",low:"9.930"
+volume:12120067,amount:121060815,ticktime:"15:00:00",per:5.429,pb:0.736,mktcap:29322728.316603,nmc:28075311.983601,
+turnoverratio:0.04313}]'
+'''
 
+import pandas as pd
+import json
 if __name__=='__main__':
-    for i in range(1,5,1):
+    for i in range(1,2,1):
         url = sinaStockUrl(i)
         stock_data = sinaStockData(url)
-        print(stock_data)
+        stock_json = parse_js(stock_data)
+        for item in stock_json:
+            pd.read_json(item,typ='series')
+
+
 
